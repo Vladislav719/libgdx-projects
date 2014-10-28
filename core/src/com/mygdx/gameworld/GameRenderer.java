0df1c5b4ae7c0501c29a940f89b16091a40b3faa
Vladislav Baimurzin <baimurzin.719@gmail.com>
2014-10-28 16:08:01 +0400

    Initial commit

---
+++ core/src/com/mygdx/gameworld/GameRenderer.java
@@ -0,0 +1,13 @@
+package com.mygdx.gameworld;
+
+import com.badlogic.gdx.Gdx;
+
+/**
+ * Created by vladislav on 28.10.14.
+ */
+public class GameRenderer {
+
+    public void render() {
+        Gdx.app.log("GameRenderer", "render");
+    }
+}
---
+++ core/src/com/mygdx/gameworld/GameWorld.java
@@ -0,0 +1,13 @@
+package com.mygdx.gameworld;
+
+import com.badlogic.gdx.Gdx;
+
+/**
+ * Created by vladislav on 28.10.14.
+ */
+public class GameWorld {
+
+    public void update(float delta) {
+        Gdx.app.log("GameWorld", "update");
+    }
+}
